0007174639406ca3a0ab5537299cc7c221fbdac5
bibekBist <107454020+backbencher8@users.noreply.github.com>
2026-07-03 19:23:48 +0545

    Create cleaned_data_from_bronze_erp_px_cat_g1v2.sql

---
+++ scripts/silver/cleaned_data_from_bronze_erp_px_cat_g1v2.sql
@@ -0,0 +1,18 @@
+Insert into silver_erp_px_cat_g1v2
+(id, cat, subcat, maintanence)
+Select
+id,
+cat,
+subcat,
+maintanence
+from bronze_erp_px_cat_g1v2;
+
+select 
+* from bronze_erp_px_cat_g1v2 
+where trim(cat) != cat
+or trim(subcat) != subcat
+or trim(maintanence) != maintanence;
+
+select distinct maintanence from bronze_erp_px_cat_g1v2;
+
+select * from silver_erp_px_cat_g1v2;
