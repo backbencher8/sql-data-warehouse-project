df85d60780a10973a37185a6bf8e212b31afccfb
bibekBist <107454020+backbencher8@users.noreply.github.com>
2026-07-06 11:01:18 +0545

    Add files via upload

---
+++ scripts/gold/gold_dim_product.sql
@@ -0,0 +1,19 @@
+create view gold_dim_product as 
+select 
+	Row_number() over (order by pr.prd_start_dt, pr.prd_key) as product_key,
+	pr.prd_id as product_id,
+	pr.prd_key as product_number,
+	pr.prd_name  as product_name,
+    pr.cat_id as category_id,
+    pc.cat as category,
+	pc.subcat as sub_category,
+    pc.maintanence,
+    pr.prd_cost as cost,
+	pr.prd_line as product_line,
+	pr.prd_start_dt as start_date
+from silver_crm_prd_info pr
+left join silver_erp_px_cat_g1v2 pc on  
+pr.cat_id = pc.id
+where pr.prd_end_dt is Null;
+
+select * from gold_dim_product;
---
+++ scripts/gold/gold_fact_sales.sql
@@ -0,0 +1,16 @@
+Create view gold_fact_sales as
+select
+sl.sls_ord_num as order_number,
+pr.product_key,
+cu.customer_key,
+sl.sls_order_dt as order_date,
+sl.sls_ship_dt as shipping_date,
+sl.sls_due_dt  as due_date,
+sl.sls_sales as sales_amount,
+sl.sls_quantity as quantity,
+sl.sls_price as price
+from silver_sales_details sl
+left join gold_dim_product pr
+on sl.sls_prd_key = pr.product_number
+left join gold_dim_customers cu
+on sl.sls_cust_id = cu.customer_id;
