ea0a8a09179c4e9debd00b593c2cd6947ceddae7
bibekBist <107454020+backbencher8@users.noreply.github.com>
2026-07-06 11:03:52 +0545

    Update gold_dim_customers.sql

--- scripts/gold/gold_dim_customers.sql
+++ scripts/gold/gold_dim_customers.sql
@@ -1,3 +1,13 @@
+/* Script Purpose:
+    This script creates views for the Gold layer in the data warehouse. 
+    The Gold layer represents the final dimension and fact tables (Star Schema)
+
+    This view performs transformations and combines data from the Silver layer 
+    to produce a clean, enriched, and business-ready dataset.
+
+Usage:
+    - These views can be queried directly for analytics and reporting.
+	*/
 Create view gold_dim_customers as 
 select 
 	ROW_NUMBER() OVER (order by customer_id) as customer_key,
