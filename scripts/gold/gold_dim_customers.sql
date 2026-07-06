b3a4136ddf2798fbc1a4daef6afe28710c5bb11b
bibekBist <107454020+backbencher8@users.noreply.github.com>
2026-07-06 11:00:19 +0545

    Create gold_dim_customers.sql

---
+++ scripts/gold/gold_dim_customers.sql
@@ -0,0 +1,57 @@
+Create view gold_dim_customers as 
+select 
+	ROW_NUMBER() OVER (order by customer_id) as customer_key,
+	ci.customer_id as customer_id,
+	ci.customer_key as customer_number,
+	ci.customer_firstname as first_name,
+	ci.customer_lastname as last_name,
+    la.cntry as country,
+	ci.customer_marital_status as marital_status,
+	CASE
+        WHEN ci.customer_gender IN ('Male', 'Female')
+            THEN ci.customer_gender
+
+        WHEN ci.customer_gender = 'Unknown'
+             AND ca.gen IN ('Male', 'Female')
+            THEN ca.gen
+
+        ELSE 'n/a'
+    END AS gender,
+    ca.bdate as birthdate,
+	ci.customer_create_date as create_date
+from silver_crm_customer_info ci
+left join silver_erp_cust_az12 ca
+on ci.customer_key = ca.cid
+left join silver_erp_loc_a101 la
+on ci.customer_key = la.cid;
+
+
+-- checking gender match or not 
+SELECT DISTINCT
+    CASE
+        WHEN ci.customer_gender IN ('Male', 'Female')
+            THEN ci.customer_gender
+
+        WHEN ci.customer_gender = 'Unknown'
+             AND ca.gen IN ('Male', 'Female')
+            THEN ca.gen
+
+        ELSE 'n/a'
+    END AS new_gen
+FROM silver_crm_customer_info ci
+LEFT JOIN silver_erp_cust_az12 ca
+    ON ci.customer_key = ca.cid
+LEFT JOIN silver_erp_loc_a101 la
+    ON ci.customer_key = la.cid
+ORDER BY 1,2;
+
+SELECT
+    customer_gender,
+    COUNT(*) AS total_customers
+FROM silver_crm_customer_info
+GROUP BY customer_gender
+HAVING COUNT(*) > 1;
+
+select distinct gender from gold_dim_customers;
+
+
